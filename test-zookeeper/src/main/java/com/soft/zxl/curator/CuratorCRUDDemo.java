29ab126bad28d803a2aa9812b0fa27183afd3cf4
zhaoxl <975378524@qq.com>
2019-01-07 15:01:28 +0800

    zookeeper练习

--- test-zookeeper/src/main/java/com/soft/zxl/curator/CuratorCRUDDemo.java
+++ test-zookeeper/src/main/java/com/soft/zxl/curator/CuratorCRUDDemo.java
@@ -1,6 +1,7 @@
 package com.soft.zxl.curator;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.data.Stat;
 
 /**
  * @program: test
@@ -17,13 +18,59 @@ public class CuratorCRUDDemo {
     public static void main(String[] args) {
         client.start();
         createNode();
+        System.out.printf("" + checkExits());
+        selectNode();
+        updateNode();
+        selectNode();
+        deleteNode();
+        System.out.printf("" + checkExits());
     }
 
-    public static void createNode(){
+    public static void createNode() {
         try {
-            client.create().forPath(PATH,"create".getBytes());
+            client.create().forPath(PATH, "create".getBytes());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void updateNode() {
+        try {
+            client.setData().forPath(PATH, "update".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectNode() {
+        try {
+            byte[] bytes = client.getData().forPath(PATH);
+            System.out.println(new String(bytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteNode() {
+        try {
+            client.delete().forPath(PATH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean checkExits() {
+        Stat stat = null;
+        try {
+            stat = client.checkExists().forPath(PATH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (null != stat) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
